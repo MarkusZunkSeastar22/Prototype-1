a803a22785890df5039f2c84e87739433b4cd1d1
MarkusZunkSeastar22 <56129192+MarkusZunkSeastar22@users.noreply.github.com>
2019-11-05 10:14:21 -0800

    1.4 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
 {
+public float speed = 5.0f;
+public float turnSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
       // we'll move the vehicle forward
-      transform.Translate(Vector3.forward);
+      transform.Translate(Vector3.forward * Time.deltaTime * speed);
+      transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
+
 }
